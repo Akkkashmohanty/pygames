c0b91b9c07fa83231e31568eda8fb34de43e79f5
Akash Kumar Mohanty <132094010+Akkkashmohanty@users.noreply.github.com>
2025-06-20 18:00:34 +0530

    commit

--- tutorial.py
+++ tutorial.py
@@ -15,7 +15,24 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HIGHT))
 
-
-def main(window):
+def get_background(name):
     pass
 
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    main(window)
