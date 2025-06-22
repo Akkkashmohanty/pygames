03739b77e8030942637940f931fca9e9c8d6450d
Akash Kumar Mohanty <132094010+Akkkashmohanty@users.noreply.github.com>
2025-06-22 12:03:44 +0530

    Object

--- tutorial.py
+++ tutorial.py
@@ -149,3 +149,12 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self, win, offset_x):
         win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
+
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
