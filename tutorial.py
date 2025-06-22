160454f970698a28b31c983ed846189329a71103
Akash Kumar Mohanty <132094010+Akkkashmohanty@users.noreply.github.com>
2025-06-22 12:01:19 +0530

    move

--- tutorial.py
+++ tutorial.py
@@ -78,3 +78,22 @@ class Player(pygame.sprite.Sprite):
         self.jump_count += 1
         if self.jump_count == 1:
             self.fall_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def make_hit(self):
+        self.hit = True
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
